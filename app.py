4c89e529bc45a6eab3f4bdcde10c945d61034298
jeffszakacs1978-afk <jeffszakacs1978@gmail.com>
2026-04-18 02:05:01 -0500

    Increase Lyapunov threshold from 30.0 to 60.0

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_signal_energy(text):
     return values if values else [0]
 
 def lyapunov_check(signal, decay=1.0):
-    threshold = 30.0
+    threshold = 60.0
     energy_path = []
     current_energy = 0
     neutralized = False
